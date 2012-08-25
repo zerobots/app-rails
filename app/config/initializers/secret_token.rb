423378b4c96c0ad3e7cffde74de70100ea4e96a0
Charles Blaxland <charles.blaxland@gmail.com>
2012-08-25 19:29:06 +1000

    Renamed web to app

--- app/config/initializers/secret_token.rb
+++ app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Web::Application.config.secret_token = 'baedbb765e24916063585b122fe32cc1d8d03565913a7eff7ca5d5d03391cec830c64475215bd28c7842f913463b71c2a66dddb979e8d54c60cf694f7e88001b'
+App::Application.config.secret_token = '38ccffc854ff699143b16a5e5306b27f72b374a02decd61992290bd6a047a0eeb7162e07de598067da425e5d437780aa30e7d0ff8fcefe6722db141bc2eccc0f'
