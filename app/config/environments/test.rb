56a9777934582d28fb580e8815a8bf444f488480
vagrant <vagrant@localhost.localdomain>
2012-10-07 10:03:43 +0200

    Adding Jquery-ui, Bootstrap, and Devise to created app

--- app/config/environments/test.rb
+++ app/config/environments/test.rb
@@ -34,4 +34,7 @@ App::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Devise
+  config.action_mailer.default_url_options = { :host => ENV['public_dns_name'] }
 end
